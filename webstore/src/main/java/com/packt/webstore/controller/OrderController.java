d4a9781db7edfe794525034a126f4a88685b0988
Marco <sir_lemus@hotmail.com>
2015-05-25 00:31:52 -0500

    delete unused import on OrderController.

--- webstore/src/main/java/com/packt/webstore/controller/OrderController.java
+++ webstore/src/main/java/com/packt/webstore/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.packt.webstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.packt.webstore.service.OrderService;
 
